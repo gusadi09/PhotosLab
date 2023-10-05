0d95ec0656970eb63919952d6b35bd53ab1986f8
Agus Adi <gusadi872@gmail.com>
2023-10-06 01:54:12 +0700

    INIT

---
+++ PhotosLab/ImagePicker.swift
@@ -0,0 +1,18 @@
+//
+//  ImagePicker.swift
+//  PhotosLab
+//
+//  Created by Gus Adi on 06/10/23.
+//
+
+import SwiftUI
+
+struct ImagePicker: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ImagePicker()
+}
---
+++ PhotosLab/NetworkMonitor.swift
@@ -0,0 +1,8 @@
+//
+//  NetworkMonitor.swift
+//  PhotosLab
+//
+//  Created by Gus Adi on 05/10/23.
+//
+
+import Foundation
